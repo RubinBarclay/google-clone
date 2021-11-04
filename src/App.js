1ef4f94ddb1bf6050090449ff3fa628344871f69
Rubin Barclay <rubinbarclay@protonmail.com>
2021-11-04 08:02:41 +0100

    removed context provider

--- src/App.js
+++ src/App.js
@@ -2,26 +2,20 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Results from "./pages/results/Results";
-import SearchContext from "./context/searchContext";
-import { useState } from "react";
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState("");
-
   return (
     <Router>
-      <SearchContext.Provider value={{ searchQuery, setSearchQuery }}>
-        <div className="App">
-          <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route path="/results/:searchQuery">
-              <Results />
-            </Route>
-          </Switch>
-        </div>
-      </SearchContext.Provider>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/results/:searchQuery">
+            <Results />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
